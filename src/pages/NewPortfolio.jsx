eb1517c1431d77bdf578253fc3c5c600fa8830df
JEONGMIN <gyflsakfn@naver.com>
2023-01-01 18:59:21 +0900

    Feat: 입력 값 상태로 저장하기

--- src/pages/NewPortfolio.jsx
+++ src/pages/NewPortfolio.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../component/ui/Button';
 
 const NewPortfolio = () => {
+  const [portfolio, setPortfolio] = useState({});
+  const [file, setFile] = useState();
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === 'file') {
+      setFile(files && files[0]);
+      return
+    }
+    setPortfolio((port) => ({ ...port, [name]: value }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
-    <div>NewPortfolio</div>
+    <section>
+      {file && <img src={URL.createObjectURL(file)} alt='local file' />}
+      <form onSubmit={handleSubmit}>
+        <input type="file" accept='image/#' name='file' required onChange={handleChange} />
+        <input type="text" name='title' value={portfolio.title ?? ''} placeholder='프로젝트명' required onChange={handleChange} />
+        <input type="text" name='category' value={portfolio.category ?? ''} placeholder='카테고리' required onChange={handleChange} />
+        <input type="text" name='demoUrl' value={portfolio.demoUrl ?? ''} placeholder='데모 사이트' required onChange={handleChange} />
+        <input type="text" name='gitUrl' value={portfolio.gitUrl ?? ''} placeholder='깃헙 주소' required onChange={handleChange} />
+        <Button text={'Submit'} />
+      </form>
+    </section>
   )
 }
 
