a68bb15143050d9c75bf69f744d24a50273a7601
JEONGMIN <gyflsakfn@naver.com>
2023-01-04 18:27:11 +0900

    Feat: project 목록 보이기
    스타일링과 구조 수정할 필요가 있음.
    업로드 날짜 있으면 좋을 듯.
    project 상태 업데이트 체크 해볼 것. 바로 업데이트 안됨

--- src/section/portfolio/Portfolio.jsx
+++ src/section/portfolio/Portfolio.jsx
@@ -7,10 +7,25 @@ import Button from '../../component/ui/Button';
 import { getProjects } from '../../api/firebase';
 import ProjectCard from './ProjectCard';
 import { useQuery } from "@tanstack/react-query";
+import { useState } from 'react';
+import { categories } from '../../util/category';
+import ProjectsCategories from './ProjectsCategories';
 
 const Portfolio = () => {
   const { isLoading, error, data: projects } = useQuery(['projects'], getProjects)
   const { user, login, logout } = useAuthContext();
+  const [filterData, setFilterData] = useState(projects);
+  const uniqueCategories = ["all", ...new Set(categories)];
+
+  const filterProjectsHandler = (category) => {
+    if (category === "all") {
+      setFilterData(projects);
+      return;
+    }
+
+    const filterProjects = projects.filter(project => project.category === category);
+    setFilterData(filterProjects);
+  }
 
   const handleLogin = () => {
     login();
@@ -26,12 +41,16 @@ const Portfolio = () => {
       <p>
         Check out some of the projects I recently worked on for my clients. Use the buttons to toggle the different categories.
       </p>
+
       {isLoading && <p>Loading..</p>}
       {error && <p>{error}</p>}
-      <ul className="portfolio__projects" >
-        {projects &&
-          projects.map(project => <ProjectCard key={project.id} project={project} />)}
-      </ul>
+      <div className="container portfolio__container">
+        <ProjectsCategories categories={uniqueCategories} onFilterProjects={filterProjectsHandler} />
+        <ul className="portfolio__projects" >
+          {filterData &&
+            filterData.map(project => <ProjectCard key={project.id} project={project} />)}
+        </ul>
+      </div>
 
       {user && <User user={user} />}
       {!user && <Button text={'Login'} onClick={handleLogin} />}
