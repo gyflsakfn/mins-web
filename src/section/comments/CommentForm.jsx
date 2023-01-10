e7540f3d3e8bf279b007a1be8905558504505a4e
JEONGMIN <gyflsakfn@naver.com>
2023-01-10 23:35:15 +0900

    reset test

--- src/section/comments/CommentForm.jsx
+++ src/section/comments/CommentForm.jsx
@@ -1,43 +1,68 @@
-import React, { useState } from 'react'
-import { addComment } from '../../api/firebase';
+
+import React, { useCallback, useRef, useState } from 'react'
+import { addNewComment } from '../../api/firebase';
 import Button from '../../component/ui/Button';
+import User from '../../component/User';
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
-
-const CommentForm = ({ user, comments }) => {
+const CommentForm = ({ user, comments, isComment }) => {
   const [commentInfo, setCommentInfo] = useState({});
-  const [isSubmit, setIsSubmit] = useState(isTrue);
+  const { setUserComment, isUserComment } = isComment;
+
+  const textRef = useRef();
+
+  const handleResizeHeight = useCallback(() => {
+    textRef.current.style.height = 'auto';
+    textRef.current.style.height = textRef.current.scrollHeight + 'px';
+  }, []);
+
+  const queryClient = useQueryClient();
+
+  const addComment = useMutation(({ commentInfo, user }) => addNewComment(commentInfo, user),
+    {
+      onSuccess: () => queryClient.invalidateQueries('projects'),
+    });
 
   const handleChange = (e) => {
-    const { name, value, checked } = e.target;
-    setCommentInfo((prev) => ({ ...prev, [name]: value, isAnon: checked }))
+    const { name, value } = e.target;
+    setCommentInfo((prev) => ({ ...prev, [name]: value }))
+    handleResizeHeight();
   }
 
-  // TODO: isSubmit 상태 이름 변경!! 고민 중...
+  const isAnonChange = (e) => {
+    const { checked } = e.target;
+    setCommentInfo((prev) => ({ ...prev, isAnon: checked }))
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    window.alert('완료')
-    addComment(commentInfo, user)
-    // setIsSubmit(true);
+    addComment.mutate({ commentInfo, user }, {
+      onSuccess: () => {
+        window.alert('코멘트가 성공적으로 추가되었습니다.')
+        setUserComment(true);
+      }
+    })
   }
 
-  function isTrue() {
-    let result = comments?.filter((comment) => comment.id === user.uid);
-    if (result.length === 0) return false;
-    else return true
-  }
+  // function isWrite() {
+  //   let result = comments?.filter((comment) => comment.id === user.uid);
+  //   if (result.length === 0) return false;
+  //   else return true
+  // }
 
   return (
     <>
       {
-        isSubmit ? <p>작성해주셔서 감사합니다.</p> :
+        isUserComment ? <p>{`${user.displayName}님 작성해주셔서 감사합니다.`}</p> :
           <form onSubmit={handleSubmit} className="commentForm">
-            <div className="input__wrapper">
-              <input type="text" name="comment" placeholder="코멘트를 적으세요." onChange={handleChange} />
+            <div className="commentForm__user-wrapper">
+              {user && <User user={user} />}
               <label htmlFor="checkAnon">익명</label>
-              <input id='checkAnon' type="checkbox" name="isAnon" onChange={handleChange} />
+              <input id='checkAnon' type="checkbox" name="isAnon" onChange={isAnonChange} />
             </div>
-            <div className="btn__wrapper">
-              <Button text='제출하기'></Button>
+            <textarea rows={1} ref={textRef} type="text" name="comment" placeholder="코멘트를 적으세요." onChange={handleChange} />
+            <div className="submit__button-wrapper">
+              <Button text='작성하기'></Button>
             </div>
           </form>
       }
