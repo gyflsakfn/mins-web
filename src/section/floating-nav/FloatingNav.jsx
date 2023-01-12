20fc8c0924a4bb45ac86eea4626ee24edd36bf0e
JEONGMIN <gyflsakfn@naver.com>
2023-01-12 19:23:33 +0900

    테블릿과 모바일에서 보일 floating-nav 구현 예정

---
+++ src/section/floating-nav/FloatingNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './floating-nav.css'
+
+const FloatingNav = () => {
+  return (
+    <section id='floating__nav'>FloatingNav</section>
+  )
+}
+
+export default FloatingNav
